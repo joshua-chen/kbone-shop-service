e930890cc33e5d1ee612492b0c7b0c74c538bca0
陈洲华 <joshua_chen@qq.com>
2020-06-04 18:11:28 +0800

    提交

--- shop_wap/routes/routes.go
+++ shop_wap/routes/routes.go
@@ -10,10 +10,10 @@
 package routes
 
 import (
-	_"commons/middleware"
-	_"commons/middleware/jwt"
-	_ "commons/mvc/route"
-	_ "commons/mvc/context/response"
+	_"github.com/joshua-chen/go-commons/middleware"
+	_"github.com/joshua-chen/go-commons/middleware/jwt"
+	_ "github.com/joshua-chen/go-commons/mvc/route"
+	_ "github.com/joshua-chen/go-commons/mvc/context/response"
 	_ "fmt"
 	_ "net/http"
 	_"shop/repositories"
